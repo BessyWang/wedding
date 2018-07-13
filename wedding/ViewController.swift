b940ea826cf2e932e6921f2f622c8045540295d2
王沛翎 <a0983455127@icloud.com>
2018-07-13 22:23:46 +0800

    Initial Commit

---
+++ wedding/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wedding
+//
+//  Created by Bessy Wang on 2018/7/13.
+//  Copyright © 2018年 Bessy Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
